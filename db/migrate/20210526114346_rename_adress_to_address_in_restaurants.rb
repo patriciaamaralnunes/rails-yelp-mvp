f993a26a6219910c878f1edce10d008aafd476ce
Patricia Nunes <patricia.amaral.nunes@hotmail.com>
2021-05-26 17:04:31 +0100

    restrevs

---
+++ db/migrate/20210526114346_rename_adress_to_address_in_restaurants.rb
@@ -0,0 +1,5 @@
+class RenameAdressToAddressInRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :restaurants, :adress, :address
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
